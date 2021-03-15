93b5b1553e3aeb0eec45d60d545ac5e64a10df74
yixianghsy <yixianghsy@gmail.com>
2021-03-15 18:18:54 +0800

    mengxuegu-cloud-oauth2 启动报错问题

--- mengxuegu-cloud-oauth2-parent/mengxuegu-cloud-oauth2-server/src/main/java/com/mengxuegu/oauth2/server/config/TokenConfig.java
+++ mengxuegu-cloud-oauth2-parent/mengxuegu-cloud-oauth2-server/src/main/java/com/mengxuegu/oauth2/server/config/TokenConfig.java
@@ -3,11 +3,12 @@ package com.mengxuegu.oauth2.server.config;
 import com.alibaba.druid.pool.DruidDataSource;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.security.oauth2.provider.token.TokenStore;
 import org.springframework.security.oauth2.provider.token.store.JdbcTokenStore;
 
 import javax.sql.DataSource;
-
+@Configuration
 public class TokenConfig {
     //@Autowired 采用redis管理token
     //private RedisConnectionFactory redisConnectionFactory;
