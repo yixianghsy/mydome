0179976806c844b99e94be9bb92896351afef946
yixianghsy <yixianghsy@gmail.com>
2021-03-17 23:09:41 +0800

    完毕

--- mengxuegu-cloud-oauth2-parent/mengxuegu-cloud-oauth2-sso-client1/src/main/java/com/mengxuegu/oauth2/sso/config/SsoSecurityConfig.java
+++ mengxuegu-cloud-oauth2-parent/mengxuegu-cloud-oauth2-sso-client1/src/main/java/com/mengxuegu/oauth2/sso/config/SsoSecurityConfig.java
@@ -1,9 +1,13 @@
 package com.mengxuegu.oauth2.sso.config;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.boot.autoconfigure.security.oauth2.resource.UserInfoRestTemplateFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
+
 @EnableOAuth2Sso
 @Configuration
 public class SsoSecurityConfig extends WebSecurityConfigurerAdapter {
@@ -23,4 +27,9 @@ public class SsoSecurityConfig extends WebSecurityConfigurerAdapter {
                 .csrf().disable()
         ;
     }
+    @Bean
+    public OAuth2RestTemplate restTemplate(UserInfoRestTemplateFactory factory) {
+        return factory.getUserInfoRestTemplate();
+    }
+
 }
--- mengxuegu-cloud-oauth2-parent/mengxuegu-cloud-oauth2-sso-client1/src/main/java/com/mengxuegu/oauth2/sso/controller/MainController.java
+++ mengxuegu-cloud-oauth2-parent/mengxuegu-cloud-oauth2-sso-client1/src/main/java/com/mengxuegu/oauth2/sso/controller/MainController.java
@@ -1,16 +1,27 @@
 package com.mengxuegu.oauth2.sso.controller;
 
+import com.mengxuegu.base.result.MengxueguResult;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.oauth2.client.OAuth2RestTemplate;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MainController {
+
+    @Autowired
+    private OAuth2RestTemplate restTemplate;
+
     @GetMapping("/")
     public String index() {
         return "index";
     }
     @GetMapping("/member")
     public String member() {
+        MengxueguResult result =
+        restTemplate.getForObject("http://localhost:7001/product/list",MengxueguResult.class);
+        System.out.println("获取商品信息：" + result);
+
         return "member";
     }
 
