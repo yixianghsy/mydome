ed4dae645654ed077f0253f10d76c9e86887875b
yixianghsy <yixianghsy@gmail.com>
2021-03-29 22:37:54 +0800

    初始化提交

--- mall-app-web/src/components/HelloWorld.vue
+++ mall-app-web/src/components/HelloWorld.vue
@@ -9,6 +9,8 @@
     <h3>Installed CLI Plugins</h3>
     <ul>
       <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-router" target="_blank" rel="noopener">router</a></li>
+      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-vuex" target="_blank" rel="noopener">vuex</a></li>
     </ul>
     <h3>Essential Links</h3>
     <ul>
@@ -39,18 +41,19 @@ export default {
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped lang="stylus">
-h3
-  margin 40px 0 0
-
-ul
-  list-style-type none
-  padding 0
-
-li
-  display inline-block
-  margin 0 10px
-
-a
-  color #42b983
+<style scoped lang="scss">
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
 </style>
--- mall-app-web/src/main.js
+++ mall-app-web/src/main.js
@@ -1,33 +1,12 @@
 import Vue from 'vue'
-import './cube-ui'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import axios from 'axios'
-import 'amfe-flexible'
-import setaxios from './setaxios'
-setaxios()
+import ElementUI from 'element-ui' //element-ui的全部组件
+import 'element-ui/lib/theme-chalk/index.css'//element-ui的css
+Vue.use(ElementUI) //使用elementUI
 Vue.config.productionTip = false
-Vue.prototype.$http=axios
 
-
-//路由守卫
-router.beforeEach((to,from,next)=>{
-  //无论是刷新还是跳转路由，第一个进入的就是这个路由前置钩子函数
-  store.commit('settoken',localStorage.getItem('token'))
-  if(to.meta.requireAuth){
-    if(store.state.token){
-      next()
-    }else{
-      next({
-        path:'/login',
-        query:{redirect:to.fullPath}
-      })
-    }
-  }else{
-    next()
-  }
-})
 new Vue({
   router,
   store,
